c2663aa70b57bb3ac7be55d059df7e3191be3e15
Ren Ant <eurekavixen@gmail.com>
2024-09-26 17:52:36 +0800

    added article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,21 @@
 class ArticlesController < ApplicationController
+
   def show
     @article = Article.find(params[:id])
-    render :json => params
+    render json: params
   end
 
   def index
     @articles = Article.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
   end
 end
