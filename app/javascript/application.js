41463ce788ccdf8a2716f7282e5b13be0bd4ac2d
Renier Antonio <eurekavixen@gmail.com>
2024-12-17 13:35:13 +0800

    [Added]: before reformat

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,4 +2,4 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-Turbo.session.drive = false
+// Turbo.session.drive = false
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_24_074910) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_01_015726) do
   create_table "articles", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "username"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
